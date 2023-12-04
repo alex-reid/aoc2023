a687fdf5fda794c4d0b912484011e1ace34e42eb
alex-reid <alex@alex-reid.com.au>
2023-12-04 23:45:55 +1100

    day 4

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -83,16 +83,16 @@ Both parts: 1.546ms
 
 ```
 Day 03
-Time part 1: 1.912ms
-Time part 2: 1.457ms
-Both parts: 3.369ms
+Time part 1: 2.159ms
+Time part 2: 1.462ms
+Both parts: 3.621ms
 ```
 
 ```
 Day 04
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.835ms
+Time part 2: 79.729ms
+Both parts: 81.564ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 9.865ms
+Total stars: 8/50
+Total time: 91.681ms
 ```
 
 <!--/RESULTS-->
--- src/day04/index.js
+++ src/day04/index.js
@@ -1,35 +1,84 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput) => rawInput;
+const parseInput = (rawInput) =>
+  rawInput.split("\n").map((v) => {
+    const nums = v.split(":")[1];
+    return nums
+      .split("|")
+      .map((v1) => v1.match(/\d+/g).map((v2) => parseInt(v2)));
+  });
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput);
+  let finalScore = 0;
 
-  return;
+  input.forEach((row) => {
+    const winning = row[0];
+    const mine = row[1];
+    let score = 0.5;
+    mine.forEach((number) => {
+      if (winning.includes(number)) {
+        score *= 2;
+      }
+    });
+    if (score >= 1) finalScore += score;
+  });
+
+  return finalScore;
 };
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
+  const cardScores = [];
 
-  return;
+  input.forEach((row, index) => {
+    const [winning, mine] = row;
+    const current = { total: 0, index };
+    mine.forEach((number) => {
+      if (winning.includes(number)) {
+        current.total += 1;
+      }
+    });
+    cardScores.push(current);
+  });
+
+  function processCards(card) {
+    let total = 1;
+    for (let i = card.index + 1; i < card.index + card.total + 1; i++) {
+      total += processCards(cardScores[i]);
+    }
+    return total;
+  }
+
+  return cardScores.reduce((a, c) => (a += processCards(c)), 0);
 };
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 13,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 30,
+      },
     ],
     solution: part2,
   },
