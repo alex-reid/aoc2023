090bee1850f7e2855f0a8c7c3fb21cf968ec319b
alex-reid <alex@alex-reid.com.au>
2023-12-09 23:51:45 +1100

    day 9

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
-![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
@@ -118,16 +118,16 @@ Both parts: 18.905ms
 
 ```
 Day 08
-Time part 1: 2.646ms
-Time part 2: 8.028ms
-Both parts: 10.674ms
+Time part 1: 2.729ms
+Time part 2: 7.177ms
+Both parts: 9.906ms
 ```
 
 ```
 Day 09
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 5.31ms
+Time part 2: 179.384ms
+Both parts: 184.694ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 16/50
-Total time: 149.853ms
+Total stars: 18/50
+Total time: 333.779ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day09/index.js
@@ -0,0 +1,79 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput) =>
+  rawInput.split("\n").map((v) =>
+    v
+      .trim()
+      .split(" ")
+      .map((v2) => parseInt(v2)),
+  );
+
+function getDifference(input, pt2, lastVal = []) {
+  lastVal.push(pt2 ? input[0] : input.at(-1));
+  if (new Set(input).size == 1) return lastVal;
+
+  const output = [];
+  for (let i = 0; i < input.length - 1; i++) {
+    output.push(input[i + 1] - input[i]);
+  }
+
+  return getDifference(output, pt2, lastVal);
+}
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+  const values = input.map((v) => getDifference(v));
+  const predicted = values.map((v) => v.reduce((a, c) => (a += c), 0));
+  return predicted.reduce((a, c) => (a += c), 0);
+};
+
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+  const values = input.map((v) => getDifference(v, true));
+  const output = [];
+  values.forEach((value) => {
+    let iter = 0;
+    value.reverse().forEach((i) => {
+      iter = i - iter;
+    });
+    output.push(iter);
+  });
+  return output.reduce((a, c) => (a += c), 0);
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`,
+        expected: 114,
+      },
+      {
+        input: `-2 4 12 17 14 -2 -36 -93 -178 -296 -452 -651 -898 -1198 -1556 -1977 -2466 -3028 -3668 -4391 -5202`,
+        expected: -6106,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `0 3 6 9 12 15`,
+        expected: -3,
+      },
+      {
+        input: `1 3 6 10 15 21`,
+        expected: 0,
+      },
+      {
+        input: `10 13 16 21 30 45`,
+        expected: 5,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
